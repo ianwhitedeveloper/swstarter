58ace43fc14a51306dc7a90f74a1d5bd33cf9c1d
Brittani <brittani1128@gmail.com>
2018-12-01 08:41:10 -0600

    moved stat into App, added react context

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -3,27 +3,61 @@ import { Route, Switch } from "react-router-dom";
 import Main from "../pages/Main/Main.js";
 import Details from "../pages/Details/Details.js";
 import "./App.css";
+import API from "../utils/API";
+import { AppContext } from "../utils/AppContext";
 
 class App extends React.Component {
-  state = {
-    data: null
+  handleSelect = event => {
+    this.setState({
+      searchQuery: event.target.value
+    });
+    console.log(event.target.value);
   };
-  // componentDidMount() {
-  //   // Call our fetch function below once the component mounts
-  //   this.callBackendAPI()
-  //     .then(res => this.setState({ data: res.express }))
-  //     .catch(err => console.log(err));
-  // }
-  // // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  // callBackendAPI = async () => {
-  //   const response = await fetch("/express_backend");
-  //   const body = await response.json();
 
-  //   if (response.status !== 200) {
-  //     throw Error(body.message);
-  //   }
-  //   return body;
-  // };
+  handleDetails = event => {
+    this.setState({ resultIndex: Number(event.target.dataset.index) });
+  };
+
+  selectQuery = () => {
+    switch (this.state.searchQuery) {
+      case "people":
+        API.getPeople(this.state.resultSearch).then(res =>
+          this.setState({ results: res.data })
+        );
+        break;
+      case "movies":
+        API.getMovies(this.state.resultSearch).then(res =>
+          this.setState({ results: res.data })
+        );
+        break;
+      default:
+        alert("Please select People or Movies!");
+    }
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      resultSearch: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.selectQuery();
+  };
+
+  state = {
+    results: [],
+    resultSearch: "",
+    resultIndex: null,
+    searchQuery: "people",
+    detailsSelection: "",
+    handleSelect: this.handleSelect,
+    selectQuery: this.selectQuery,
+    handleInputChange: this.handleInputChange,
+    handleSubmit: this.handleSubmit,
+    handleDetails: this.handleDetails
+  };
 
   render() {
     return (
@@ -32,10 +66,12 @@ class App extends React.Component {
           <h2>SWStarter</h2>
         </header>
         <div className="container">
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <Route path="/details" component={Details} />
-          </Switch>
+          <AppContext.Provider value={this.state}>
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route path="/details" component={Details} />
+            </Switch>
+          </AppContext.Provider>
         </div>
       </div>
     );
--- client/src/pages/Details/Details.js
+++ client/src/pages/Details/Details.js
@@ -1,35 +1,73 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Details.css";
+import { AppContext } from "../../utils/AppContext";
 
 class Details extends React.Component {
+  static contextType = AppContext;
+  renderInfo = () => {
+    const context = this.context;
+    // const result = context.results[context.resultIndex];
+    if (context.searchQuery === "people") {
+      return (
+        <div className="people-info">
+          <h3 id="title">Boba Fett</h3>
+          <div className="info">
+            <div className="details">
+              <h4>Details</h4>
+              <hr />
+              <ul>
+                <li>birthday</li>
+                <li>gender</li>
+                <li>eye</li>
+                <li>hair</li>
+                <li>heigh</li>
+                <li>mass</li>
+              </ul>
+            </div>
+
+            <div className="movies">
+              <h4>Movies</h4>
+              <hr />
+              <ul>
+                <li>
+                  <a href="#">Return of the Jedi</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="movie-info">
+          <h3 id="title">A New Hope</h3>
+          <div className="info">
+            <div className="details">
+              <h4>Opening Crawl</h4>
+              <hr />
+              <p>A long time ago in a galaxy far away...</p>
+            </div>
+
+            <div className="movies">
+              <h4>Characters</h4>
+              <hr />
+              <ul>
+                <li>
+                  <a href="#">Luke Skywalker</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };
+
   render() {
     return (
       <div className="details-wrapper">
-        <h3 id="title">Boba Fett</h3>
-        <div className="info">
-          <div className="details">
-            <h4>Details</h4>
-            <hr />
-            <ul>
-              <li>birthday</li>
-              <li>gender</li>
-              <li>eye</li>
-              <li>hair</li>
-              <li>heigh</li>
-              <li>mass</li>
-            </ul>
-          </div>
-          <div className="movies">
-            <h4>Movies</h4>
-            <hr />
-            <ul>
-              <li>
-                <a href="#">Return of the Jedi</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        {this.renderInfo()}
         <Link to={"./"}>
           <button id="back-btn" variant="raised">
             Back to Search
